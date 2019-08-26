bd8389f542d7b3b07959a1f9fbdf753235ffb519
sufyankhatri <sufyankhatree@gmail.com>
2019-08-26 17:45:17 +0500

    starting

---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+  constructor(public navCtrl: NavController) {}
+}
---
+++ src/pages/item-details/item-details.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-item-details',
+  templateUrl: 'item-details.html'
+})
+export class ItemDetailsPage {
+  constructor() {}
+}
